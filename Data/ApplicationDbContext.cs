847bf977af7c285fd9f439ba57e7943322deb446
Anmol Brar <brarsanmol@protonmail.com>
2021-08-07 09:25:38 -0400

    🗑  remove starter files and add dependencies.
    - remove starter files from the dotnet core project and react project.
      - test files, weatherforecast.
    - add dotnet core dependencies.
      - dotnet entity framework core tools.
      - dotnet entity framework design.
    - translate existing JS files to functional components.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace The_Unsent_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using The_Unsent_Project.Data;
 
 namespace The_Unsent_Project
 {
@@ -27,6 +29,9 @@ namespace The_Unsent_Project
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
